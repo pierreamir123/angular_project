bb57163626afa80ba60fefe6521a20fac3ac7f2f
pierreamir123 <pierre.farag00@eng-st.cu.edu.eg>
2022-09-12 13:24:13 +0200

    12/9/2022

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent  {
+
+  serverID: number = 10;
+  serverStatus: string = 'offline';
+
+  getServerStatus(){
+
+  }
+
+}
